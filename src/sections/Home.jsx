4cd69b31524246ab64b619ec774b32273b182451
Oslonline <oslo418@yandex.ru>
2024-03-27 10:13:05 +0100

    fix: add padding-top to Home section

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -3,7 +3,7 @@ import { Github, Medium, Linkedin, X, Location, Availability } from "../componen
 
 const Home = () => {
     return (
-        <section className="flex w-full flex-col items-center gap-20 py-10 md:flex-row-reverse md:py-24">
+        <section className="pt-20 flex w-full flex-col items-center gap-20 py-10 md:flex-row-reverse md:py-24">
             <div className="flex items-center justify-center md:w-1/3">
                 <div className="w-3/6 md:h-2/5 md:w-3/5">
                     <img id="main-img" src={MainImg} alt="" className="hidden border-8 border-transparent shadow-mlight dark:shadow-mdark md:block" />
