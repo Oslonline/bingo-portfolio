73fc84df72a1a9f5b8a4908334e59de8b0bce2ea
Oslonline <oslo418@yandex.ru>
2024-03-27 10:12:33 +0100

    feat: add backdrop-blur to navbar & burger menu background

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -34,38 +34,50 @@ const NavBar = () => {
     };
 
     return (
-        <header className="sticky top-0 z-10 flex h-20 w-full items-center justify-between bg-white px-4 dark:bg-gray-950 md:justify-around">
-            <div>
-                <a href="#" onClick={() => handleNavItemClick(null)} className="select-none text-3xl font-krona dark:text-gray-50">
-                    Bingo
-                </a>
-            </div>
-            <div className="hidden items-center gap-5 md:flex">
-                <nav className="flex gap-5 text-gray-600 dark:text-gray-300">
-                    <a href="#articles" onClick={() => handleNavItemClick("1")} className={`scroll-smooth rounded-xl px-3 py-2 hover:bg-gray-200 hover:text-gray-900 active:bg-gray-300 ${activeNavItem === "1" ? "bg-gray-200 text-gray-900 dark:bg-gray-700 dark:text-gray-50" : ""}`}>
-                        Articles
+        <>
+            <header className="sticky top-0 z-10 hidden h-20 w-full items-center justify-between bg-white/75 px-4 backdrop-blur-xl dark:bg-gray-950/75 md:flex md:justify-around">
+                <div>
+                    <a href="#" onClick={() => handleNavItemClick(null)} className="select-none font-krona text-3xl dark:text-gray-50">
+                        Bingo
                     </a>
-                    <a href="#projects" onClick={() => handleNavItemClick("2")} className={`scroll-smooth rounded-xl px-3 py-2 hover:bg-gray-200 hover:text-gray-900 active:bg-gray-300 ${activeNavItem === "2" ? "bg-gray-200 text-gray-900 dark:bg-gray-700 dark:text-gray-50" : ""}`}>
-                        Projects
-                    </a>
-                    <a href="#stacks" onClick={() => handleNavItemClick("3")} className={`scroll-smooth rounded-xl px-3 py-2 hover:bg-gray-200 hover:text-gray-900 active:bg-gray-300 ${activeNavItem === "3" ? "bg-gray-200 text-gray-900 dark:bg-gray-700 dark:text-gray-50" : ""}`}>
-                        Stacks
-                    </a>
-                </nav>
-                <div className="flex items-center gap-5">
-                    <hr className="w-5 rotate-90" />
-                    <button onClick={() => setIsDarkMode(!isDarkMode)}>
-                        <img src={isDarkMode ? darkModeIcon : lightModeIcon} alt="" />
-                    </button>
-                    <a href="mailto:therealbingops@gmail.com" className="rounded-xl bg-gray-900 px-5 py-2 text-gray-50 hover:bg-gray-700 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-gray-50">Contact Me</a>
                 </div>
-            </div>
-            <div className="flex items-center justify-center md:hidden">
-                <button onClick={handleBurgerClick} className="focus:outline-none ">
-                    {burgerIcon ? <BurgerClose /> : <BurgerOpen />}
-                </button>
-                {/* Burger Menu */}
-                <div className={`absolute left-0 top-16 z-20 w-full bg-white shadow-md transition-all duration-300 dark:bg-gray-950 ${showBurgerMenu ? "opacity-100" : "pointer-events-none opacity-0"}`}>
+                <div className="hidden items-center gap-5 md:flex">
+                    <nav className="flex gap-5 text-gray-600 dark:text-gray-200">
+                        <a href="#articles" onClick={() => handleNavItemClick("1")} className={`scroll-smooth rounded-xl px-3 py-2 hover:bg-gray-200 hover:text-gray-900 active:bg-gray-300 ${activeNavItem === "1" ? "bg-gray-200 text-gray-900 dark:bg-gray-700 dark:text-gray-50" : ""}`}>
+                            Articles
+                        </a>
+                        <a href="#projects" onClick={() => handleNavItemClick("2")} className={`scroll-smooth rounded-xl px-3 py-2 hover:bg-gray-200 hover:text-gray-900 active:bg-gray-300 ${activeNavItem === "2" ? "bg-gray-200 text-gray-900 dark:bg-gray-700 dark:text-gray-50" : ""}`}>
+                            Projects
+                        </a>
+                        <a href="#stacks" onClick={() => handleNavItemClick("3")} className={`scroll-smooth rounded-xl px-3 py-2 hover:bg-gray-200 hover:text-gray-900 active:bg-gray-300 ${activeNavItem === "3" ? "bg-gray-200 text-gray-900 dark:bg-gray-700 dark:text-gray-50" : ""}`}>
+                            Stacks
+                        </a>
+                    </nav>
+                    <div className="flex items-center gap-5">
+                        <hr className="w-5 rotate-90" />
+                        <button onClick={() => setIsDarkMode(!isDarkMode)}>
+                            <img src={isDarkMode ? darkModeIcon : lightModeIcon} alt="" />
+                        </button>
+                        <a href="mailto:therealbingops@gmail.com" className="rounded-xl bg-gray-900 px-5 py-2 text-gray-50 hover:bg-gray-700 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-gray-50">
+                            Contact Me
+                        </a>
+                    </div>
+                </div>
+            </header>
+            <header className="fixed top-0 z-20 w-full bg-white/75 px-4 backdrop-blur-xl dark:bg-gray-950/75 md:hidden">
+                <div className="flex h-20 items-center justify-between">
+                    <div>
+                        <a href="#" onClick={() => handleNavItemClick(null)} className="select-none font-krona text-3xl dark:text-gray-50">
+                            Bingo
+                        </a>
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <button onClick={handleBurgerClick} className="focus:outline-none ">
+                            {burgerIcon ? <BurgerClose /> : <BurgerOpen />}
+                        </button>
+                    </div>
+                </div>
+                <div className={`flex w-full flex-col shadow-md transition-all duration-300 ${showBurgerMenu ? "opacity-100" : "pointer-events-none hidden opacity-0"}`}>
                     <div className="flex w-full p-5">
                         <div className="flex w-1/2">
                             <nav className="flex flex-col gap-4 text-gray-600 dark:text-gray-300">
@@ -96,7 +108,9 @@ const NavBar = () => {
                                 >
                                     Stacks
                                 </a>
-                                <a href="mailto:therealbingops@gmail.com" className="rounded-xl bg-gray-900 px-5 py-2 text-gray-50 hover:bg-gray-700 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-gray-50">Contact Me</a>
+                                <a href="mailto:therealbingops@gmail.com" className="rounded-xl bg-gray-900 px-5 py-2 text-gray-50 hover:bg-gray-700 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-gray-50">
+                                    Contact Me
+                                </a>
                             </nav>
                         </div>
                         <div className="flex w-1/2 items-end justify-end pb-2">
@@ -106,8 +120,8 @@ const NavBar = () => {
                         </div>
                     </div>
                 </div>
-            </div>
-        </header>
+            </header>
+        </>
     );
 };
 
