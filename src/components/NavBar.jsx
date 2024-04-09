b4d75eee884612d34d5eae476a2cafbbbf72fa2e
Oslonline <oslo418@yandex.ru>
2024-04-09 22:41:29 +0200

    feat: add contact form emailjs implementation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import emailjs from "@emailjs/browser";
 import darkModeIcon from "../assets/icons/dark-mode.svg";
 import lightModeIcon from "../assets/icons/light-mode.svg";
 import { BurgerOpen, BurgerClose } from "./icons";
@@ -12,6 +13,26 @@ const NavBar = () => {
     });
     const [showBurgerMenu, setShowBurgerMenu] = useState(false);
     const [burgerIcon, setBurgerIcon] = useState(false);
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm("Gmail_portfolio", "template_1puw1na", form.current, {
+                publicKey: "xYSscmGX_B3W6WcjC",
+            })
+            .then(
+                () => {
+                    console.log("Mail send with success !");
+                },
+                (error) => {
+                    console.log("Mail failed to leave your computer something's wrong...", error.text);
+                },
+            );
+        closeModal();
+    };
 
     useEffect(() => {
         localStorage.setItem("darkMode", JSON.stringify(isDarkMode));
@@ -43,11 +64,6 @@ const NavBar = () => {
 
     const closeModal = () => {
         setIsOpen(false);
-    };
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        closeModal();
     };
 
     return (
@@ -147,18 +163,18 @@ const NavBar = () => {
                                 <BurgerClose />
                             </button>
                         </div>
-                        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+                        <form ref={form} onSubmit={sendEmail} className="flex flex-col gap-4">
                             <div className="flex flex-col gap-1">
                                 <label className="select-none text-sm text-gray-700 dark:text-gray-300" htmlFor="username">
                                     Full Name :
                                 </label>
-                                <input placeholder="John Doe" className="w-full rounded-lg border border-gray-500 bg-gray-50 p-2 focus:border-gray-900 focus:outline-none dark:border-transparent dark:bg-gray-700 dark:text-gray-50 dark:caret-white dark:focus:border dark:focus:border-gray-300" name="username" type="text" required />
+                                <input placeholder="John Doe" className="w-full rounded-lg border border-gray-500 bg-gray-50 p-2 focus:border-gray-900 focus:outline-none dark:border-transparent dark:bg-gray-700 dark:text-gray-50 dark:caret-white dark:focus:border dark:focus:border-gray-300" name="user_name" type="text" required />
                             </div>
                             <div className="flex flex-col gap-1">
                                 <label className="select-none text-sm text-gray-700 dark:text-gray-300" htmlFor="email">
                                     Email :
                                 </label>
-                                <input placeholder="john.doe@mail.com" className="w-full rounded-lg border border-gray-500 bg-gray-50 p-2 focus:border-gray-900 focus:outline-none dark:border-transparent dark:bg-gray-700 dark:text-gray-50 dark:caret-white dark:focus:border dark:focus:border-gray-300" name="email" type="email" required />
+                                <input placeholder="john.doe@mail.com" className="w-full rounded-lg border border-gray-500 bg-gray-50 p-2 focus:border-gray-900 focus:outline-none dark:border-transparent dark:bg-gray-700 dark:text-gray-50 dark:caret-white dark:focus:border dark:focus:border-gray-300" name="user_email" type="email" required />
                             </div>
                             <div className="flex flex-col gap-1">
                                 <label className="select-none text-sm text-gray-700 dark:text-gray-300" htmlFor="message" required>
@@ -166,7 +182,7 @@ const NavBar = () => {
                                 </label>
                                 <textarea placeholder="I want to talk with you about ....." name="message" className="w-full rounded-lg border border-gray-500 bg-gray-50 p-2 focus:border-gray-900 focus:outline-none dark:border-transparent dark:bg-gray-700 dark:text-gray-50 dark:caret-white dark:focus:border dark:focus:border-gray-300" rows="7"></textarea>
                             </div>
-                            <button type="submit" className="rounded-lg bg-gray-900 px-4 py-2  text-gray-50 hover:bg-gray-700 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-gray-50">
+                            <button type="submit" value="Send" className="rounded-lg bg-gray-900 px-4 py-2  text-gray-50 hover:bg-gray-700 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-gray-50">
                                 Submit
                             </button>
                         </form>
