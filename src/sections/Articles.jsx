90bf6384a7cfd46fda44708ca523ec9e4640f21a
Oslonline <Oslo418@yandex.ru>
2025-04-13 18:02:28 +0200

    feat: remove unnecessary React import for cleaner code

--- src/sections/Articles.jsx
+++ src/sections/Articles.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Article from "../components/Article";
 import articlesData from "../articles.json";
 
